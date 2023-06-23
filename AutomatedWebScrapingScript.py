7063526102e552ed162867f3f04ccc4ae7a92137
Nirav <101149413+Ni1292@users.noreply.github.com>
2023-06-23 00:41:06 -0400

    finished script

--- AutomatedWebScrapingScript.py
+++ AutomatedWebScrapingScript.py
@@ -1,7 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.scrapethissite.com/pages/simple/'
+url = 'https://scrapingclub.com/exercise/list_basic/'
 response = requests.get(url)
-soup = BeautifulSoup(response.text, 'lxml')
-print(soup)
+soup = BeautifulSoup(response.text,features= "html.parser")
+items = soup.findAll('div',class_='col-lg-4 col-md-6 mb-4')
+count =1
+for i in items:
+    itemName = soup.find('h4',class_='card-title').text.strip('\n')
+    itemPrice = soup.find('h5').text
+    count = count + 1
+
+pages = soup.find('ul',class_='pagination')
+urls=[]
+links = soup.find_all('a',class_='page-link')
+for link in links:
+    pageNumb = int(link.text) if link.text.isdigit() else None
+    if pageNumb != None:
+        x = link.get('href')
+        urls.append(x)
+for i in urls:
+    newURL = url + i
+    response = requests.get(newURL)
+    soup = BeautifulSoup(response.text, features="html.parser")
+    items = soup.findAll('div', class_='col-lg-4 col-md-6 mb-4')
+    count = 1
+    for i in items:
+        itemName = soup.find('h4', class_='card-title').text.strip('\n')
+        itemPrice = soup.find('h5').text
+        print(f"{count}, Price : {itemPrice}, Name :{itemName}")
+        count = count + 1
+
