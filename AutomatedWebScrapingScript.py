e365c01eee221c7ba44fae75c0a14f576bd4fde4
Nirav <101149413+Ni1292@users.noreply.github.com>
2023-06-23 15:59:20 -0400

    Added export data into excel file functionality

--- AutomatedWebScrapingScript.py
+++ AutomatedWebScrapingScript.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from openpyxl.workbook import Workbook
 
+
+data= []
 url = 'https://scrapingclub.com/exercise/list_basic/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text,features= "html.parser")
@@ -15,11 +18,13 @@ for i in items:
 pages = soup.find('ul',class_='pagination')
 urls=[]
 links = soup.find_all('a',class_='page-link')
+# Go thru all page itself and extract data.
 for link in links:
     pageNumb = int(link.text) if link.text.isdigit() else None
     if pageNumb != None:
         x = link.get('href')
         urls.append(x)
+# Scaping all data from file
 for i in urls:
     newURL = url + i
     response = requests.get(newURL)
@@ -27,10 +32,16 @@ for i in urls:
     items = soup.findAll('div', class_='col-lg-4 col-md-6 mb-4')
     count = 1
     for i in items:
-        itemName = soup.find('h4', class_='card-title').text.strip('\n')
-        itemPrice = soup.find('h5').text
+        item={}
+        item['itemName'] = soup.find('h4', class_='card-title').text.strip('\n')
+        item['itemPrice'] = soup.find('h5').text
         print(f"{count}, Price : {itemPrice}, Name :{itemName}")
+        data.append(item)
         count = count + 1
+# export data into excel file
+def export(data):
+    df = pd.DataFrame(data)
+    df.to_excel("scraped_data_file.xlsx")
 
 
-
+export(data)
